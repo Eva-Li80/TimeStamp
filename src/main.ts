869a6116865b5eb1bece5e733650d00d3e95d65f
Eva-Li80 <evaliboden@gmail.com>
2024-01-22 14:53:26 +0100

    page five eva

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+
 
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -8,6 +8,9 @@ import { startPage } from "./tsPages/startPage";
 import { startTimer } from "./tsPages/numberToTextPage";
 startTimer(1, 5);
 
+import { getTimerBar } from "./tsPages/timerBarPage";
+getTimerBar();
+
 import { createTimer } from "./tsPages/startTimer.ts";
 
 
