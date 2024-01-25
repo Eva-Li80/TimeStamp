a611cfe66077a829eb5324ea07ea4dca4ef76988
isabellaes <borjessonisabella@gmail.com>
2024-01-25 11:06:29 +0100

    close meny on click

--- src/typescript/tsPages/Nav-Bar.ts
+++ src/typescript/tsPages/Nav-Bar.ts
@@ -5,6 +5,29 @@ export function NavBar() {
   const menyCloseIcon = document.getElementById(
     "menu-close-icon"
   ) as HTMLElement;
+  const visualTimer = document.getElementById(
+    "menu-VISUAL_TIMER"
+  ) as HTMLElement;
+  const analogTimer = document.getElementById(
+    "menu-ANALOG_TIMER"
+  ) as HTMLElement;
+  const digitalTimer = document.getElementById(
+    "menu-DIGITAL_TIMER"
+  ) as HTMLElement;
+  const textTimer = document.getElementById("menu-TEXT_TIMER") as HTMLElement;
+  const circleTimer = document.getElementById(
+    "menu-CIRCLES_TIMER"
+  ) as HTMLElement;
+  const startTimer = document.getElementById(
+    "menu-start-timer1"
+  ) as HTMLElement;
+  visualTimer.addEventListener("click", toggleMenu);
+  analogTimer.addEventListener("click", toggleMenu);
+  digitalTimer.addEventListener("click", toggleMenu);
+  textTimer.addEventListener("click", toggleMenu);
+  circleTimer.addEventListener("click", toggleMenu);
+  startTimer.addEventListener("click", toggleMenu);
+
   //ikonen på startsidan
   menuIcon.addEventListener("click", toggleMenu);
   //ikon mobilvy öppna menyn
